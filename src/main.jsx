001c5be53e248d2a61484297a380a5073cd599b1
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-03-22 04:39:50 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SignupPage from './pages/SignupPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <LoginPage/> */}
+    <SignupPage/>
   </StrictMode>,
 )
