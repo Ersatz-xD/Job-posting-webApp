1ac789fd4d6343bb163c832276bb8461382b7fae
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-03-22 22:25:05 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,12 @@ import React from 'react'
 import JobPost from './components/job-post/JobPost'
 import { jobPosts } from '../testData'
 import AddJobComp from './components/add-job-comp/AddJobComp'
+import HomePage from './pages/HomePage'
 
 
 export const App = () => {
   return (
-    <div className='appParent'>
-
-      <AddJobComp/>
-      {
-        jobPosts.map((item) => (
-          <JobPost title = {item.title} desc={item.desc} role={item.role} email={item.email}/>
-
-        ))
-      }
-    </div>
+    <HomePage/>
   )
 }
 
