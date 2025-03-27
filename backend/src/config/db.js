78e43ed7dc8c219892e18fce989a55b1953fd472
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-03-28 03:02:01 +0500

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose"
+import User from "../models/user.model.js";
 
 const connectDB = async () => {
     try {
       await  mongoose.connect(process.env.MONGO_URI);
       console.log("Database conneted successfully!");
+    
       
     } catch (error) {
-        console.log('Error accored: $(error)');
+        console.log(`Error accored: ${error}`);
         
     }
 }
+ 
 
 export default connectDB;
