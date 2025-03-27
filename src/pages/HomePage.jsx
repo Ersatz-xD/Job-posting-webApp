1e4a5b6363be6546379d27f3eba85504d97b7530
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-03-27 21:24:28 +0500

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import AddJobComp from '../components/add-job-comp/AddJobComp'
 import { jobPosts } from '../../testData'
 import JobPost from '../components/job-post/JobPost'
+import axios from 'axios'
 
 
 export const HomePage = () => {
+
+    const fetchJobs = () => {
+        await axios.get()
+    }
   return (
     <div className='appParent'>
 
