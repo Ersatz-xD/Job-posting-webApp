892c771e9012d6ef3978f65b08ccd22e3038f12c
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-03-28 03:02:18 +0500

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,8 @@ dotenv.config();
 
 connectDB().then( ()=> {
     app.listen(process.env.PORT, ()=> {
-        console.log(`Server is rnning on port: $(process.env.PORT)`);
+        console.log(`Server is running on port: ${process.env.PORT}`);
+
         
     });
 })
